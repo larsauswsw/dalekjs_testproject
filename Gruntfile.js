f802a464419a5a4a32e24e8b50b9d324d2e3d327
Lars Miesner <lars.miesner@etecture.de>
2014-02-18 11:27:23 +0100

    first real commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+	  dalek: {
+		dist: {
+		  src: ['test/my_first_test.js']
+		},
+		options: {
+		  //browser: ['phantomjs', 'firefox', 'chrome', 'IE'],
+		  browser: ['phantomjs'],
+		  reporter: ['console', 'html', 'junit', 'json'],
+		  // don't load config from an Dalekfile
+		  dalekfile: false,
+		  // specify advanced options (that else would be in your Dalekfile)
+		  advanced: {
+			browsers: [{
+				firefox: {
+					binary: 'C:/Program Files (x86)/Mozilla Firefox/firefox.exe'
+				}
+			}]/*,
+			'html-reporter': {
+			dest: 'reports/html'
+		  },
+		  'junit-reporter': {
+			dest: 'reports/junit'
+		  }*/
+		  }
+		}
+	  }
+
+	});
+
+	/**
+	 * Loads tasks located in the tasks directory.
+	 */
+	grunt.loadNpmTasks('grunt-dalek');
+	grunt.registerTask('default', ['dalek']);
+};
---
+++ test/my_first_test.js
@@ -0,0 +1,18 @@
+module.exports = {
+'Page title is correct': function (test) {
+  test
+    .open('http://www.google.de')
+    .assert.title().is('Google', 'It has title')
+    .done();
+},
+
+'Amazon does its thing': function (test) {
+  test
+    .open('http://www.amazon.com/')
+    .type('#twotabsearchtextbox', 'Blues Brothers VHS')
+    .click('.nav-submit-input')
+    .waitForElement('#result_0')
+    .assert.text('#result_0 .newaps a span').is('The Blues Brothers [VHS]')
+    .done();
+}
+};
