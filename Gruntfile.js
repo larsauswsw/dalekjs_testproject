5d92ee2cc0e19c70ea30c66d65a0d08e51bd809e
Lars Miesner <lars.miesner@etecture.de>
2014-05-13 10:03:07 +0200

    added some file to gitignore, set some more reports

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,26 +6,28 @@ module.exports = function(grunt) {
 		  src: ['test/*.js']
 		},
 		options: {
-		  //browser: ['phantomjs', 'firefox', 'chrome', 'IE'],
-		  browser: ['phantomjs', 'chrome'],
-		  //browser: ['phantomjs', 'firefox', 'chrome' ],
-		  //reporter: ['console', 'html', 'junit', 'json'],
+		  
+		  browser: ['phantomjs', 'chrome' ],
+		  reporter: ['console', 'html', 'junit', 'json'],
 		  // don't load config from an Dalekfile
 		  dalekfile: false,
 		  // specify advanced options (that else would be in your Dalekfile)
-		  advanced: {
-			/*browsers: [{
+		  advanced: {/*
+			browsers: [{
 				firefox: {
 					binary: 'C:/Program Files (x86)/Mozilla Firefox/firefox.exe'
 				}
-			}]*//*,
+			}],*/
 			'html-reporter': {
 			dest: 'reports/html'
-		  },
-		  'junit-reporter': {
-			dest: 'reports/junit'
-		  }*/
-		  }
+			  },
+			  'junit-reporter': {
+				dest: 'reports/junit'
+			  },
+			  'json-reporter': {
+				  dest: 'reports/json/result.json'
+				}
+			  }
 		}
 	  }
 
